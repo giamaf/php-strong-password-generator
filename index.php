add800fe6dc7b58312679667e246aa75bd88b58c
giamaf <gianluca.maffucci@hotmail.com>
2024-02-21 15:34:02 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+$page_label = 'PHP Strong Password Generator';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?= $page_label ?></title>
+</head>
+
+<body>
+
+</body>
+
+</html>
---
+++ readme.md
@@ -0,0 +1,5 @@
+# PHP Strong Password Generator
+
+Creo una pagina in grado di generare una password per gli utenti.<br>
+L'utente potrà scegliere la lunghezza password e ricevere in un alert una password con il numero di caratteri casuali da lui richiesto!
+
